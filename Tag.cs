924abff56c2720d840de9dfe6444ac4ba2b319e1
vahid ara <vahidaara@gmail.com>
2022-09-29 11:11:52 +0330

    write up tagMange  class

---
+++ Tag.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Tag 
+{
+    public static string player_TAG = "";
+    public static string ENEMY_TAG = "";
+    public static string GAMEPLAYE_CAVES_TAG = "";
+
+    public static string HORIZENTAL_AXIS = "Horizontal";
+    public static string VERTICAL_aXIS = "Vertical";
+
+    public static string IDEL_ANIMATION_NAME = "";
+    public static string WALK_ANIMATION_NAME = "";
+    public static string DEATH_ANIMATION_NAME = "";
+    public static string SHOOT_ANIMATION_NAME = "";
+    public static string ATTACK_ANIMATION_NAME = "";
+
+
+
+
+
+
+}
